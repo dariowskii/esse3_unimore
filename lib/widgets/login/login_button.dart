215d83c095459d8d0fcf9e12189961d19953af2c
dariowskii <dariovarriale@rocketmail.com>
2021-05-08 19:07:22 +0200

    Refactor widget login

---
+++ lib/widgets/login/login_button.dart
@@ -0,0 +1,29 @@
+import 'package:Esse3/constants.dart';
+import 'package:flutter/material.dart';
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({
+    Key key,
+    @required this.onPressed,
+  }) : super(key: key);
+  final Function() onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      disabledColor: Constants.buttonDisabled,
+      onPressed: onPressed,
+      padding: const EdgeInsets.all(16),
+      color: Constants.mainColor,
+      textColor: Colors.white,
+      disabledTextColor: Colors.black26,
+      minWidth: double.infinity,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+      ),
+      child: const Text(
+        'ACCEDI',
+        style: Constants.fontBold,
+      ),
+    );
+  }
+}
---
+++ lib/widgets/login/login_form.dart
@@ -0,0 +1,221 @@
+import 'package:Esse3/constants.dart';
+import 'package:Esse3/screens/home_screen.dart';
+import 'package:Esse3/utils/provider.dart';
+import 'package:Esse3/widgets/login/login_button.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  bool _isLoading = false;
+
+  final _userController = TextEditingController();
+  final _passController = TextEditingController();
+  final FocusNode _passFocus = FocusNode();
+
+  /// Serve per effettuare il login grazie a [Provider.getAccess()].
+  Future<void> _login() async {
+    setState(() {
+      _isLoading = !_isLoading;
+    });
+    FocusScope.of(context).unfocus();
+
+    if (_userController.text.isEmpty || _passController.text.isEmpty) {
+      setState(() {
+        _isLoading = !_isLoading;
+        _passController.clear();
+      });
+      return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Attenzione!'),
+            content: const Text('Riempi correttamente tutti i campi'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  _passController.clear();
+                  Navigator.of(context).pop();
+                },
+                child: const Text(
+                  'Chiudi',
+                  style: TextStyle(color: Constants.mainColorLighter),
+                ),
+              ),
+            ],
+          );
+        },
+      );
+    }
+    final responseSession =
+        await Provider.getSession(_userController.text, _passController.text);
+    if (responseSession == null) {
+      setState(() {
+        _isLoading = !_isLoading;
+        _userController.clear();
+        _passController.clear();
+      });
+      await showDialog(
+        context: context,
+        builder: (BuildContext ctx) {
+          return AlertDialog(
+            title: const Text('Errore di connessione'),
+            content:
+                const Text("Riprova a effettuare l'accesso fra 30 secondi."),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop();
+                },
+                child: const Text(
+                  'Chiudi',
+                  style: TextStyle(color: Constants.mainColorLighter),
+                ),
+              ),
+            ],
+          );
+        },
+      );
+    } else if (responseSession['success'] as bool) {
+      var prefs = await SharedPreferences.getInstance();
+
+      await prefs.setBool('isLoggedIn', true);
+      await prefs.setString('username', _userController.text);
+      await prefs.setString('password', _passController.text);
+
+      final userInfo = await Provider.getHomeInfo();
+      if (!(userInfo['success'] as bool)) {
+        _errorSession();
+        return;
+      }
+      setState(() {
+        _isLoading = !_isLoading;
+      });
+      await Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomeScreen(user: userInfo),
+        ),
+      );
+    } else if (!(responseSession['success'] as bool)) {
+      _errorSession();
+    }
+  }
+
+  void _errorSession() {
+    setState(() {
+      _isLoading = !_isLoading;
+      _userController.clear();
+      _passController.clear();
+    });
+    showDialog(
+      context: context,
+      builder: (BuildContext ctx) {
+        return AlertDialog(
+          title: const Text('Credenziali errate!'),
+          content: const Text('Riprova a inserire le credenziali'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(ctx).pop();
+              },
+              child: const Text('Chiudi',
+                  style: TextStyle(color: Constants.mainColorLighter)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _userController.dispose();
+    _passController.dispose();
+    _passFocus.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 10,
+            spreadRadius: 2,
+          ),
+        ],
+        color: Colors.white,
+      ),
+      padding: const EdgeInsets.all(24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Usa le credenziali Esse3 per accedere.',
+            style: Constants.font16.copyWith(
+              color: Colors.black87,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Column(
+            children: [
+              TextField(
+                enabled: !_isLoading,
+                controller: _userController,
+                maxLength: 15,
+                keyboardType: TextInputType.emailAddress,
+                cursorColor: Constants.mainColor,
+                decoration: const InputDecoration(
+                  labelText: 'Username',
+                  counterText: '',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                ),
+                onSubmitted: (_) => _passFocus.requestFocus(),
+              ),
+              const SizedBox(height: 15),
+              TextField(
+                enabled: !_isLoading,
+                obscureText: true,
+                focusNode: _passFocus,
+                controller: _passController,
+                cursorColor: Constants.mainColor,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(color: Colors.redAccent, width: 2),
+                  ),
+                ),
+                onSubmitted: (_) => _login(),
+              ),
+            ],
+          ),
+          Visibility(
+              visible: _isLoading,
+              child: Column(
+                children: const [
+                  SizedBox(height: 20),
+                  LinearProgressIndicator(
+                    backgroundColor: Colors.transparent,
+                    valueColor:
+                        AlwaysStoppedAnimation<Color>(Constants.mainColor),
+                  ),
+                ],
+              )),
+          const SizedBox(height: 20),
+          LoginButton(onPressed: !_isLoading ? () => _login() : null),
+        ],
+      ),
+    );
+  }
+}
