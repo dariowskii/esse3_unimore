477412353987c51dc28328e7a388eb6708989f06
dariowskii <dario.varriale@atobit.it>
2021-08-10 22:23:24 +0200

    Added static funcs

--- lib/constants.dart
+++ lib/constants.dart
@@ -70,4 +70,161 @@ class Constants {
   // Widgets
   static const SizedBox sized10H = SizedBox(height: 10);
   static const SizedBox sized20H = SizedBox(height: 20);
+  // Functions
+  static void showAdaptiveWaitingDialog(BuildContext context) {
+    final content = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+        CircularProgressIndicator.adaptive(
+          valueColor: AlwaysStoppedAnimation<Color>(Constants.mainColorLighter),
+        ),
+        SizedBox(width: 10),
+        Text(
+          'Attendi un secondo...',
+          style: TextStyle(fontStyle: FontStyle.italic),
+        ),
+      ],
+    );
+
+    showModalBottomSheet(
+        context: context,
+        isDismissible: false,
+        enableDrag: false,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+        ),
+        builder: (_) {
+          return SizedBox(
+            height: 180,
+            child: content,
+          );
+        });
+  }
+
+  static void showAdaptiveDialog(BuildContext context,
+      {@required bool success,
+      @required String successText,
+      @required String errorText}) {
+    final content = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              success ? Icons.check_circle : Icons.error,
+              color: success ? Colors.green : Colors.redAccent,
+            ),
+            const SizedBox(width: 15),
+            Text(
+              success ? successText : errorText,
+              style: Constants.fontBold18,
+            ),
+          ],
+        ),
+        const SizedBox(height: 20),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: MaterialButton(
+            color: success ? Colors.green : Colors.redAccent,
+            minWidth: 150,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            onPressed: () =>
+                Navigator.of(context).popUntil((route) => route.isFirst),
+            child: const Text(
+              'Chiudi',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+    showModalBottomSheet(
+      context: context,
+      isDismissible: false,
+      enableDrag: false,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (_) {
+        return SizedBox(
+          height: 200,
+          child: content,
+        );
+      },
+    );
+  }
+
+  static Widget getAskingModalBottomSheet(BuildContext context,
+      {@required Function() onAccepted,
+      @required String title,
+      @required String text}) {
+    return SizedBox(
+      height: 180,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: Constants.fontBold20,
+          ),
+          const SizedBox(height: 5),
+          Text(
+            text,
+            style: const TextStyle(fontFamily: 'SF Pro'),
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Flexible(
+                  child: MaterialButton(
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    minWidth: double.maxFinite,
+                    color: Colors.green,
+                    onPressed: onAccepted,
+                    child: const Text(
+                      'Si',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Flexible(
+                  child: MaterialButton(
+                    minWidth: double.maxFinite,
+                    color: Colors.redAccent,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text(
+                      'No',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
